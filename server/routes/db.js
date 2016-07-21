b782cd63bb75a17ef7292869b5e318b728aab5b3
bpeterson1663 <bpeterson1663@gmail.com>
2016-07-20 22:20:47 -0500

    updating database to be saved locally

---
+++ server/routes/db.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var MongoURI =  'mongodb://heroku_d7cmdhsv:bsjdqkvm75potj28f372j9pgpo@ds011311.mlab.com:11311/heroku_d7cmdhsv' || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL  || 'mongodb://localhost/music_vidoes';
+
+
+var MongoDB = mongoose.connect(MongoURI).connection;
+
+MongoDB.on("error", function(err){
+  console.log("There was an error with Mongo");
+});
+
+MongoDB.once("open", function(){
+  console.log("MongoDB Up and Running");
+});
+
+module.exports = MongoDB;
